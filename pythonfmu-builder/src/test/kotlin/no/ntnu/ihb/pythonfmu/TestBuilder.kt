8acba7c63f4c353948ca6f88aa4d5860bcbf00da
Lars Ivar Hatledal <laht@ntnu.no>
2019-10-14 10:24:02 +0200

    Use fmi2slave.py from root dir

--- pythonfmu-builder/src/test/kotlin/no/ntnu/ihb/pythonfmu/TestBuilder.kt
+++ pythonfmu-builder/src/test/kotlin/no/ntnu/ihb/pythonfmu/TestBuilder.kt
@@ -14,7 +14,7 @@ class TestBuilder {
         val dest = "build/generated"
         val scriptFile = File(TestBuilder::class.java.classLoader.getResource("pythonslave.py").file).absolutePath
 
-        FmuBuilder.main(arrayOf("-f", scriptFile, "-c", "PythonSlave", "-d", dest))
+        FmuBuilder.main(arrayOf("-f", scriptFile, "-c", "PythonSlave", "-d", dest, "../fmi2slave.py"))
 
         val fmuFile = File(dest, "PythonSlave.fmu")
         Assertions.assertTrue(fmuFile.exists())
