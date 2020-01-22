f613d7a54a00dcdf326a1381e4826d5c0e95c994
Frederic Collonval <frederic.collonval@safrangroup.com>
2020-01-22 07:52:24 -0500

    Add cmake script in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,75 @@
-from setuptools import setup
+import os
+import shutil
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            # '-DPYTHON_EXECUTABLE=' + sys.executable
+        ]
+
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += [
+                # '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)
+            ]
+            # if sys.maxsize > 2**32:
+            #     cmake_args += ['-A', 'x64']
+            # build_args += ['--', '/m']
+        else:
+            cmake_args += [
+                # '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                "-DCMAKE_BUILD_TYPE="
+                + cfg
+            ]
+            # build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
+
+        if os.path.exists(self.build_temp):
+            shutil.rmtree(self.build_temp)
+        os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+
 
 # See setup.cfg for Python package parameters
-setup()
+setup(
+    ext_modules=[CMakeExtension("pythonfmu.export", "pythonfmu-export")],
+    cmdclass=dict(build_ext=CMakeBuild),
+)
