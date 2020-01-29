8487d2813fc96abffb1c2f0640d844a54811fced
Frederic Collonval <frederic.collonval@safrangroup.com>
2020-01-29 09:20:00 -0500

    Add a simple test

--- pythonfmu/tests/pythonslave.py
+++ pythonfmu/tests/pythonslave.py
@@ -1,7 +1,5 @@
 from pythonfmu.fmi2slave import Fmi2Slave, Fmi2Causality, Integer, Real, Boolean, String
 
-from random import random as randn
-
 slave_class = "PythonSlave"  # REQUIRED - Name of the class extending Fmi2Slave
 
 
@@ -18,11 +16,11 @@ class PythonSlave(Fmi2Slave):
         self.realOut = 3.0
         self.booleanVariable = True
         self.stringVariable = "Hello World!"
-        self.register_variable(Integer("intOut").set_causality(Fmi2Causality.output))
-        self.register_variable(Real("realOut").set_causality(Fmi2Causality.output))
-        self.register_variable(Boolean("booleanVariable").set_causality(Fmi2Causality.local))
-        self.register_variable(String("stringVariable").set_causality(Fmi2Causality.local))
+        self.register_variable(Integer("intOut", causality=Fmi2Causality.output))
+        self.register_variable(Real("realOut", causality=Fmi2Causality.output))
+        self.register_variable(Boolean("booleanVariable", causality=Fmi2Causality.local))
+        self.register_variable(String("stringVariable", causality=Fmi2Causality.local))
 
     def do_step(self, current_time, step_size):
-        self.realOut = randn()
+        self.realOut = current_time + step_size
         return True
--- pythonfmu/tests/test_builder.py
+++ pythonfmu/tests/test_builder.py
@@ -5,10 +5,8 @@ import pytest
 
 from pythonfmu.builder import FmuBuilder
 
-try:
-    import pyfmi
-except ImportError:
-    pyfmi = None
+
+pyfmi = pytest.importorskip("pyfmi", reason="pyfmi is required for testing the produced FMU")
 
 DEMO = "pythonslave.py"
 
@@ -22,7 +20,7 @@ def test_rawbuilder():
         fmus = list(Path(tmp_dir).rglob("*.fmu"))
 
         assert len(fmus) == 1
-
-        pyfmi = pytest.importorskip("pyfmi", reason="pyfmi is required for testing the produced FMU")
         model = pyfmi.load_fmu(str(fmus[0]))
         res = model.simulate(final_time=2.)
+
+        assert res['realOut'][-1] == pytest.approx(res['time'][-1], rel=1e-7)
