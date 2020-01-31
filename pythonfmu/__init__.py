f82f95ac7f2b31ba05d177025985b1ed5c241bb5
Frederic Collonval <fcollonval@gmail.com>
2020-01-31 10:59:36 +0100

    Add FmuBuilder in package init

--- pythonfmu/__init__.py
+++ pythonfmu/__init__.py
@@ -2,3 +2,4 @@ from ._version import __version__
 from .enums import Fmi2Causality, Fmi2Initial, Fmi2Variability
 from .variables import Boolean, Integer, Real, String
 from .fmi2slave import Fmi2Slave
+from .builder import FmuBuilder
