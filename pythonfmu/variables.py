24781e86fea5c5b0218a634f452f7d99dce6045b
Frederic Collonval <fcollonval@gmail.com>
2020-02-06 20:22:42 +0100

    Correct variables.py

--- pythonfmu/variables.py
+++ pythonfmu/variables.py
@@ -7,73 +7,70 @@ from .enums import Fmi2Causality, Fmi2Initial, Fmi2Variability
 
 
 class ScalarVariable(ABC):
-
     def __init__(
-        self, 
+        self,
         name: str,
-        causality: Optional[Fmi2Causality]=None, 
-        description: Optional[str]=None, 
-        initial: Optional[Fmi2Initial]=None,
-        variability: Optional[Fmi2Variability]=None,
+        causality: Optional[Fmi2Causality] = None,
+        description: Optional[str] = None,
+        initial: Optional[Fmi2Initial] = None,
+        variability: Optional[Fmi2Variability] = None,
     ):
         self.__attrs = {
-            'name': name,
-            'valueReference': None,
-            'description': description,
-            'causality': causality,
-            'variability': variability,
-            'initial': initial,
+            "name": name,
+            "valueReference": None,
+            "description": description,
+            "causality": causality,
+            "variability": variability,
+            "initial": initial,
             # 'canHandleMultipleSetPerTimeInstant': # Only for ME
         }
 
     @property
     def causality(self) -> Optional[Fmi2Causality]:
-        return self.__attrs['causality']
+        return self.__attrs["causality"]
 
     @property
     def description(self) -> Optional[str]:
-        return self.__attrs['description']
+        return self.__attrs["description"]
 
     @property
     def initial(self) -> Optional[Fmi2Initial]:
-        return self.__attrs['initial']
+        return self.__attrs["initial"]
 
     @property
     def name(self) -> str:
-        return self.__attrs['name']
+        return self.__attrs["name"]
 
     @property
     def value_reference(self) -> int:
-        return self.__attrs['valueReference']
+        return self.__attrs["valueReference"]
 
     @value_reference.setter
     def value_reference(self, value: int):
-        if self.__attrs['valueReference'] is not None:
+        if self.__attrs["valueReference"] is not None:
             raise RuntimeError("Value reference already set.")
-        self.__attrs['valueReference'] = value
+        self.__attrs["valueReference"] = value
 
     @property
     def variability(self) -> Optional[Fmi2Variability]:
-        return self.__attrs['variability']
+        return self.__attrs["variability"]
 
     def to_xml(self) -> Element:
         attrib = dict()
         for key, value in self.__attrs.items():
             if value is not None:
                 attrib[key] = str(value.name if isinstance(value, Enum) else value)
-        return Element('ScalarVariable', attrib)
+        return Element("ScalarVariable", attrib)
 
 
 class Real(ScalarVariable):
-    def __init__(self, name: str, start: Optional[Any]=None, **kwargs):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
         super().__init__(name, **kwargs)
-        self.__attrs = {
-            'start': start
-        }
+        self.__attrs = {"start": start}
 
     @property
     def start(self) -> Optional[Any]:
-        return self.__attrs['start']
+        return self.__attrs["start"]
 
     def to_xml(self) -> Element:
         attrib = dict()
@@ -81,21 +78,19 @@ class Real(ScalarVariable):
             if value is not None:
                 attrib[key] = str(value)
         parent = super().to_xml()
-        SubElement(parent, 'Real', attrib)
+        SubElement(parent, "Real", attrib)
 
         return parent
 
 
 class Integer(ScalarVariable):
-    def __init__(self, name: str, start: Optional[Any]=None, **kwargs):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
         super().__init__(name, **kwargs)
-        self.__attrs = {
-            'start': start
-        }
+        self.__attrs = {"start": start}
 
     @property
     def start(self) -> Optional[Any]:
-        return self.__attrs['start']
+        return self.__attrs["start"]
 
     def to_xml(self) -> Element:
         attrib = dict()
@@ -103,21 +98,19 @@ class Integer(ScalarVariable):
             if value is not None:
                 attrib[key] = str(value)
         parent = super().to_xml()
-        SubElement(parent, 'Integer', attrib)
+        SubElement(parent, "Integer", attrib)
 
         return parent
 
 
 class Boolean(ScalarVariable):
-    def __init__(self, name: str, start: Optional[Any]=None, **kwargs):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
         super().__init__(name, **kwargs)
-        self.__attrs = {
-            'start': start
-        }
+        self.__attrs = {"start": start}
 
     @property
     def start(self) -> Optional[Any]:
-        return self.__attrs['start']
+        return self.__attrs["start"]
 
     def to_xml(self) -> Element:
         attrib = dict()
@@ -125,21 +118,19 @@ class Boolean(ScalarVariable):
             if value is not None:
                 attrib[key] = str(value)
         parent = super().to_xml()
-        SubElement(parent, 'Boolean', attrib)
+        SubElement(parent, "Boolean", attrib)
 
         return parent
 
 
 class String(ScalarVariable):
-    def __init__(self, name: str, start: Optional[Any]=None, **kwargs):
+    def __init__(self, name: str, start: Optional[Any] = None, **kwargs):
         super().__init__(name, **kwargs)
-        self.__attrs = {
-            'start': start
-        }
+        self.__attrs = {"start": start}
 
     @property
     def start(self) -> Optional[Any]:
-        return self.__attrs['start']
+        return self.__attrs["start"]
 
     def to_xml(self) -> Element:
         attrib = dict()
@@ -147,6 +138,6 @@ class String(ScalarVariable):
             if value is not None:
                 attrib[key] = str(value)
         parent = super().to_xml()
-        SubElement(parent, 'String', attrib)
+        SubElement(parent, "String", attrib)
 
         return parent
