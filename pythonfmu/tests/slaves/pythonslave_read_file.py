c898a461de7da9936150a3a7479efbf4b81df038
Lars Ivar Hatledal <laht@ntnu.no>
2021-02-25 13:36:53 +0100

    set variability so start is applied

--- pythonfmu/tests/slaves/pythonslave_read_file.py
+++ pythonfmu/tests/slaves/pythonslave_read_file.py
@@ -1,4 +1,4 @@
-from pythonfmu.fmi2slave import Fmi2Slave, Fmi2Causality, String
+from pythonfmu.fmi2slave import Fmi2Slave, Fmi2Causality, Fmi2Variability, String
 
 
 class PythonSlaveReadFile(Fmi2Slave):
@@ -9,7 +9,10 @@ class PythonSlaveReadFile(Fmi2Slave):
         with (open(f'{self.resources}/hello.txt', 'r')) as file:
             data = file.read()
 
-        self.register_variable(String("file_content", getter=lambda: data, causality=Fmi2Causality.output))
+        self.register_variable(
+            String("file_content", getter=lambda: data,
+                   causality=Fmi2Causality.output,
+                   variability=Fmi2Variability.constant))
 
     def do_step(self, current_time, step_size):
         return True
