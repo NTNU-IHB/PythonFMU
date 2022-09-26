972f5fd3ebe8a47dd3ed89d4421d21433d2aa640
Lars Ivar Hatledal <laht@ntnu.no>
2022-09-26 14:45:57 +0200

    test

--- pythonfmu/tests/test_logger.py
+++ pythonfmu/tests/test_logger.py
@@ -138,8 +138,8 @@ class {name}(Fmi2Slave):
     # Load the model
     callbacks = fmpy.fmi2.fmi2CallbackFunctions()
     callbacks.logger = fmpy.fmi2.fmi2CallbackLoggerTYPE(logger)
-    callbacks.allocateMemory = fmpy.fmi2.fmi2CallbackAllocateMemoryTYPE(fmpy.fmi2.allocateMemory)
-    callbacks.freeMemory = fmpy.fmi2.fmi2CallbackFreeMemoryTYPE(fmpy.fmi2.freeMemory)
+    callbacks.allocateMemory = fmpy.fmi2.fmi2CallbackAllocateMemoryTYPE(fmpy.calloc)
+    callbacks.freeMemory = fmpy.fmi2.fmi2CallbackFreeMemoryTYPE(fmpy.free)
 
     model_description = fmpy.read_model_description(fmu)
     unzip_dir = fmpy.extract(fmu)
