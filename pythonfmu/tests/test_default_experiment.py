e6815577e49944f43f57bfea336f9aea0de969d7
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2025-05-06 23:23:43 +0200

    fix default_experiment as classvar and fix test doing nothing

--- pythonfmu/tests/test_default_experiment.py
+++ pythonfmu/tests/test_default_experiment.py
@@ -9,9 +9,14 @@ def test_default_experiment(tmp_path):
         "fmpy", reason="fmpy is not available for testing the produced FMU"
     )
 
-    script_file = Path(__file__).parent / "slaves/pythonslave_read_file.py"
-    project_file = Path(__file__).parent / "data/hello.txt"
-    fmu = FmuBuilder.build_FMU(script_file, project_files=[project_file], dest=tmp_path, needsExecutionTool="false")
+    script_file = Path(__file__).parent / "slaves/pythonslave_default_ex.py"
+    fmu = FmuBuilder.build_FMU(script_file, dest=tmp_path, needsExecutionTool="false")
     assert fmu.exists()
 
     model_description = fmpy.read_model_description(fmu)
+    default_experiment = model_description.defaultExperiment
+
+    assert default_experiment.startTime == pytest.approx(1.0)
+    assert default_experiment.stopTime == pytest.approx(2)
+    assert default_experiment.tolerance == pytest.approx(1e-3)
+    assert default_experiment.stepSize == pytest.approx(1e-3)
